44a56bb363bdac3628856ee21bc3f10b5beb0d11
KubkoFrano <franojakub@gmail.com>
2021-10-05 10:13:57 +0200

    ADD: game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] string firstScene;
+
+    private void Start()
+    {
+        App.gameManager = this;
+        StartSceneLoading("UIScene");
+        StartSceneLoading(firstScene);
+    }
+
+    IEnumerator LoadSelectedScene(string sceneName)
+    {
+        AsyncOperation loading = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+        while (!loading.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    IEnumerator UnloadSelectedScene(string sceneName)
+    {
+        AsyncOperation loading = SceneManager.UnloadSceneAsync(sceneName);
+        while (!loading.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    public void StartSceneLoading(string sceneName)
+    {
+        StartCoroutine(LoadSelectedScene(sceneName));
+    }
+
+    public void StartSceneUnloading(string sceneName)
+    {
+        StartCoroutine(UnloadSelectedScene(sceneName));
+    }
+}

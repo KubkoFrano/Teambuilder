bc8a917c6288e883d398c67c9abcf4b8a2ef2291
KubkoFrano <franojakub@gmail.com>
2021-10-08 16:23:13 +0200

    ADD: generate employee

---
+++ Assets/Scripts/Behaviours/EmployeeGenerator.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmployeeGenerator : MonoBehaviour
+{ 
+    [Tooltip("Modifies income per second")]
+    [SerializeField] int modifier;
+
+    [Header("Employee qualities generation from -100 to 200")]
+    [SerializeField] int lowest1;
+    [SerializeField] int highest1;
+
+    [Header("Employee qualities generation from -100 to 100")]
+    [SerializeField] int lowest2;
+    [SerializeField] int highest2;
+
+    public Employee GenerateEmployee()
+    {
+        Employee temp = new Employee(Random.Range(lowest1, highest1), Random.Range(lowest1, highest1),
+            Random.Range(lowest2, highest2), Random.Range(lowest2, highest2), Random.Range(lowest2, highest2), (EmpType) Random.Range(0, 3));
+
+        return temp;
+    }
+}
